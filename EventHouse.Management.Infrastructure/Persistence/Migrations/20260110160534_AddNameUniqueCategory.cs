2ee1e9079552c4d2b4c4c9a3deb8802834c31c5b
fpgutierrez <federingutierrez@gmail.com>
2026-01-11 13:51:20 +0100

    refactor:  add unique name of table "Artist"

---
+++ EventHouse.Management.Infrastructure/Persistence/Migrations/20260110160534_AddNameUniqueCategory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventHouse.Management.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameUniqueCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Artists_Name",
+                table: "Artists");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artists_Name",
+                table: "Artists",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Artists_Name",
+                table: "Artists");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artists_Name",
+                table: "Artists",
+                column: "Name");
+        }
+    }
+}
