cc0fb7bc012ce7aea938b2419d14b40daa461da3
fpgutierrez <federingutierrez@gmail.com>
2026-01-15 13:42:08 +0100

    chore: add swagger examples for artist endpoints

---
+++ EventHouse.Management.Api/Swagger/Examples/Contracts/Artists/ArtistPagedResultExample.cs
@@ -0,0 +1,31 @@
+﻿using EventHouse.Management.Api.Contracts.Artists;
+using EventHouse.Management.Api.Contracts.Common;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace EventHouse.Management.Api.Swagger.Examples.Contracts.Artists;
+
+public sealed class ArtistPagedResultExample : IExamplesProvider<PagedResult<Artist>>
+{
+    public PagedResult<Artist> GetExamples() => new()
+    {
+        Items =
+        [
+            new() {
+                Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                Name = "The Rolling Stones",
+                Category = ArtistCategory.Band
+            }
+        ],
+        TotalCount = 1,
+        Page = 1,
+        PageSize = 20,
+
+        // Include in another branch for improved clarity 
+        //Links = new PaginationLinks
+        //{
+        //    Self = "/api/v1/artists?page=1&pageSize=20",
+        //    First = "/api/v1/artists?page=1&pageSize=20",
+        //    Last = "/api/v1/artists?page=1&pageSize=20"
+        //}
+    };
+}
---
+++ EventHouse.Management.Api/Swagger/Examples/Contracts/Artists/CreateArtistRequestExample.cs
@@ -0,0 +1,13 @@
+﻿using EventHouse.Management.Api.Contracts.Artists;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace EventHouse.Management.Api.Swagger.Examples.Contracts.Artists;
+
+public sealed class CreateArtistRequestExample : IExamplesProvider<CreateArtistRequest>
+{
+    public CreateArtistRequest GetExamples() => new()
+    {
+        Name = "The Rolling Stones",
+        Category = ArtistCategory.Band
+    };
+}
