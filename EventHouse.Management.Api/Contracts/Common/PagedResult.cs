fbbab31073e3f3bd4a1d033d27a4d72708bf4f97
fpgutierrez <federingutierrez@gmail.com>
2026-01-15 14:25:00 +0100

    chore: standardize paged API responses to return empty collections

--- EventHouse.Management.Api/Contracts/Common/PagedResult.cs
+++ EventHouse.Management.Api/Contracts/Common/PagedResult.cs
@@ -3,7 +3,7 @@ namespace EventHouse.Management.Api.Contracts.Common;
 
 public sealed class PagedResult<T>
 {
-    public List<T> Items { get; init; } = [];
+    public IReadOnlyList<T> Items { get; init; } = [];
     public int TotalCount { get; init; }
     public int Page { get; init; }
     public int PageSize { get; init; }
