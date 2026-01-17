e5689e58d48b5e9157561f84a4cf717d3e0898fd
fpgutierrez <federingutierrez@gmail.com>
2026-01-17 13:09:06 +0100

    feat: add API mappers for event contracts

---
+++ EventHouse.Management.Api/Mappers/Events/EventMapper.cs
@@ -0,0 +1,26 @@
+﻿using EventHouse.Management.Api.Contracts.Common;
+using EventHouse.Management.Api.Mappers.Common;
+using EventHouse.Management.Api.Mappers.Enums;
+using EventHouse.Management.Application.Common.Pagination;
+using EventHouse.Management.Application.DTOs;
+using Contract = EventHouse.Management.Api.Contracts.Events.Event;
+
+namespace EventHouse.Management.Api.Mappers.Events;
+
+public static class EventMapper
+{
+    public static Contract ToContract(EventDto dto)
+    {
+        return new Contract
+        {
+            Id = dto.Id,
+            Name = dto.Name,
+            Description = dto.Description,
+            Scope = EventScopeMapper.ToContractRequired(dto.Scope),
+        };
+    }
+
+    public static PagedResult<Contract> ToContract(
+    PagedResultDto<EventDto> paged, HttpRequest request)
+    => paged.ToContract(ToContract, request);
+}
---
+++ EventHouse.Management.Api/Mappers/Events/EventSortMapper.cs
@@ -0,0 +1,16 @@
+﻿using EventHouse.Management.Api.Contracts.Events;
+using EventHouse.Management.Application.Queries.Events.GetAll;
+
+namespace EventHouse.Management.Api.Mappers.Events;
+
+public static class EventSortMapper
+{
+    public static EventSortField? ToApplication(EventSortBy? sortBy)
+        => sortBy switch
+        {
+            EventSortBy.Name => EventSortField.Name,
+            EventSortBy.Description => EventSortField.Description,
+            EventSortBy.Scope => EventSortField.Scope,
+            _ => null
+        };
+}
