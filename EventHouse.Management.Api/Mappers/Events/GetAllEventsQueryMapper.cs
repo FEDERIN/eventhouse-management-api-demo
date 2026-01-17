309b3a059e5b1e2068ed534d503c0c9c0e3d6989
fpgutierrez <federingutierrez@gmail.com>
2026-01-17 14:54:03 +0100

    refactor: centralize event query mapping logic

---
+++ EventHouse.Management.Api/Mappers/Events/GetAllEventsQueryMapper.cs
@@ -0,0 +1,20 @@
+﻿using EventHouse.Management.Api.Contracts.Events;
+using EventHouse.Management.Api.Mappers.Enums;
+using EventHouse.Management.Application.Queries.Events.GetAll;
+
+namespace EventHouse.Management.Api.Mappers.Events;
+
+public static class GetAllEventsQueryMapper
+{
+    public static GetAllEventsQuery FromContract(GetEventsRequest request)
+        => new()
+        {
+            Name = request.Name,
+            Description = request.Description,
+            Scope = EventScopeMapper.ToApplicationOptional(request.Scope),
+            Page = request.Page,
+            PageSize = request.PageSize,
+            SortBy = EventSortMapper.ToApplication(request.SortBy),
+            SortDirection = SortDirectionMapper.ToApplication(request.SortDirection)
+        };
+}
