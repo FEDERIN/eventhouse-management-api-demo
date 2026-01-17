611a4d135ea64f127f5da3295c490463046148cc
fpgutierrez <federingutierrez@gmail.com>
2026-01-17 12:49:31 +0100

    feat: add event API contracts

---
+++ EventHouse.Management.Api/Contracts/Events/EventScope.cs
@@ -0,0 +1,23 @@
+﻿namespace EventHouse.Management.Api.Contracts.Events;
+
+/// <summary>
+/// Geographical scope of the event.
+/// Serialized as string in JSON.
+/// </summary>
+public enum EventScope : byte
+{
+    /// <summary>
+    /// Event takes place in a single city or region.
+    /// </summary>
+    Local = 1,
+
+    /// <summary>
+    /// Event takes place in multiple cities within the same country.
+    /// </summary>
+    National = 2,
+
+    /// <summary>
+    /// Event takes place in multiple countries at the same time.
+    /// </summary>
+    International = 3
+}
---
+++ EventHouse.Management.Api/Contracts/Events/EventSortBy.cs
@@ -0,0 +1,11 @@
+﻿namespace EventHouse.Management.Api.Contracts.Events;
+
+public enum EventSortBy
+{
+    Name,
+    Description,
+    Scope
+}
+
+
+
