bbe7a449b018fe18f0f110eb0d0614c6037b5477
fpgutierrez <federingutierrez@gmail.com>
2026-01-18 13:31:31 +0100

    chore: restrict swagger successful responses to application/json

---
+++ EventHouse.Management.Api/Swagger/Filters/JsonOnlyResponsesOperationFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace EventHouse.Management.Api.Swagger.Filters;
+
+public sealed class JsonOnlyResponsesOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        foreach (var response in operation.Responses.Values)
+        {
+            if (response.Content is null || response.Content.Count == 0)
+                continue;
+
+            if (response.Content.ContainsKey("application/json"))
+            {
+                response.Content.Remove("text/plain");
+                response.Content.Remove("text/json");
+            }
+        }
+    }
+}
