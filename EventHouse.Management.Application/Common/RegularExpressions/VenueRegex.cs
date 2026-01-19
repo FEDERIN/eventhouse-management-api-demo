168ebfc33cf559d84cd77aea90b6da25b068b941
fpgutierrez <federingutierrez@gmail.com>
2026-01-19 13:50:56 +0100

    feat: add venue update validator and shared country code rule

---
+++ EventHouse.Management.Application/Common/RegularExpressions/VenueRegex.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EventHouse.Management.Application.Common.RegularExpressions;
+
+public partial class VenueRegex
+{
+    [GeneratedRegex("^[A-Z]{2}$")]
+    public static partial Regex CountryCode();
+}
