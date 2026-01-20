e6d6407256c4ddf3466319429e060ff3765d0a71
fpgutierrez <federingutierrez@gmail.com>
2026-01-20 12:55:05 +0100

    refactor: introduce application exceptions for not found and conflict

---
+++ EventHouse.Management.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace EventHouse.Management.Application.Exceptions;
+
+public sealed class NotFoundException(string entity, object id) : Exception($"{entity} with id '{id}' was not found.")
+{
+    public string Code { get; } = $"{entity.ToUpperInvariant()}_NOT_FOUND";
+    public string Title { get; } = $"{entity} not found";
+}
