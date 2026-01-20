e476513a6872e36df85e6f334692298e09c79e21
fpgutierrez <federingutierrez@gmail.com>
2026-01-20 10:00:58 +0100

    feat: add API mappers for genre contracts

---
+++ EventHouse.Management.Api/Mappers/Genres/GenreMapper.cs
@@ -0,0 +1,23 @@
+﻿using EventHouse.Management.Api.Contracts.Common;
+using EventHouse.Management.Api.Contracts.Genres;
+using EventHouse.Management.Api.Mappers.Common;
+using EventHouse.Management.Application.Common.Pagination;
+using EventHouse.Management.Application.DTOs;
+
+namespace EventHouse.Management.Api.Mappers.Genres
+{
+    public class GenreMapper
+    {
+        public static Genre ToContract(GenreDto dto)
+        {
+            return new Genre
+            {
+                Id = dto.Id,
+                Name = dto.Name
+            };
+        }
+        public static PagedResult<Genre> ToContract(
+        PagedResultDto<GenreDto> paged, HttpRequest request)
+        => paged.ToContract(ToContract, request);
+    }
+}
