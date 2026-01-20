db85a06f86f8277696a7ac98e2dc42028e9e887a
fpgutierrez <federingutierrez@gmail.com>
2026-01-20 10:22:51 +0100

    chore: document genre endpoints with swagger examples

---
+++ EventHouse.Management.Api/Swagger/Examples/Contracts/Genres/UpdateGenreRequestExample.cs
@@ -0,0 +1,12 @@
+﻿using EventHouse.Management.Api.Contracts.Genres;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace EventHouse.Management.Api.Swagger.Examples.Contracts.Genres;
+
+public sealed class UpdateGenreRequestExample : IExamplesProvider<UpdateGenreRequest>
+{
+    public UpdateGenreRequest GetExamples() => new()
+    {
+        Name = "Rock and Roll"
+    };
+}
---
+++ EventHouse.Management.Api/Swagger/Examples/Requests/Genres/GetGenresRequestExample.cs
@@ -0,0 +1,17 @@
+﻿using EventHouse.Management.Api.Contracts.Common;
+using EventHouse.Management.Api.Contracts.Genres;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace EventHouse.Management.Api.Swagger.Examples.Requests.Genres;
+
+public sealed class GetGenresRequestExample : IExamplesProvider<GetGenresRequest>
+{
+    public GetGenresRequest GetExamples() => new()
+    {
+        Name = "Rock",
+        Page = 1,
+        PageSize = 15,
+        SortBy = GenreSortBy.Name,
+        SortDirection = SortDirection.Asc
+    };
+}
