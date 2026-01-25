e42299ba991d560e82cdb5e1a9557c3f8368e7c4
fpgutierrez <federingutierrez@gmail.com>
2026-01-25 15:29:45 +0100

    feat: add application command to remove genre from artist

---
+++ EventHouse.Management.Application/Commands/Artists/RemoveGenre/RemoveArtistGenreCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EventHouse.Management.Application.Commands.Artists.RemoveGenre;
+
+internal sealed class RemoveArtistGenreCommandValidator
+    : ArtistGenreCommandValidatorBase<RemoveArtistGenreCommand>
+{
+    public RemoveArtistGenreCommandValidator()
+    {
+        ApplyArtistGenreRules(
+            cmd => cmd.ArtistId,
+            cmd => cmd.GenreId);
+    }
+}
