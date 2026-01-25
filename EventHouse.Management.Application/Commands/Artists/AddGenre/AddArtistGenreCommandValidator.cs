ef965a8de982c36a11f16a575548b6bb9dc54f64
fpgutierrez <federingutierrez@gmail.com>
2026-01-25 15:20:43 +0100

    feat: add application command to add genre to artist

---
+++ EventHouse.Management.Application/Commands/Artists/AddGenre/AddArtistGenreCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace EventHouse.Management.Application.Commands.Artists.AddGenre;
+
+internal sealed class AddArtistGenreCommandValidator
+    : ArtistGenreCommandValidatorBase<AddArtistGenreCommand>
+{
+    public AddArtistGenreCommandValidator()
+    {
+        ApplyArtistGenreRules(
+            cmd => cmd.ArtistId,
+            cmd => cmd.GenreId);
+
+        RuleFor(x => x.Status)
+            .IsInEnum().WithMessage("Status must be a valid ArtistGenreStatus value.");
+    }
+}
+
