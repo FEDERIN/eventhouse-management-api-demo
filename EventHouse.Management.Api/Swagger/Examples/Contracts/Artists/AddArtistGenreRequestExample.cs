ff33b6d4e8d6aa12c056dbd1664a74775e1a547d
fpgutierrez <federingutierrez@gmail.com>
2026-01-25 16:39:08 +0100

    feat: expose artist-genre integration endpoints

---
+++ EventHouse.Management.Api/Swagger/Examples/Contracts/Artists/AddArtistGenreRequestExample.cs
@@ -0,0 +1,16 @@
+﻿using EventHouse.Management.Api.Contracts.Artists;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace EventHouse.Management.Api.Swagger.Examples.Contracts.Artists
+{
+    internal sealed class AddArtistGenreRequestExample
+        : IExamplesProvider<AddArtistGenreRequest>
+    {
+        public AddArtistGenreRequest GetExamples() => new()
+        {
+            GenreId = Guid.Parse("22222222-2222-2222-2222-222222222222"),
+            IsPrimary = true,
+            Status = ArtistGenreStatus.Active
+        };
+    }
+}
---
+++ EventHouse.Management.Application/DTOs/ArtistGenreDto.cs
@@ -0,0 +1,12 @@
+﻿
+using EventHouse.Management.Application.Common.Enums;
+
+namespace EventHouse.Management.Application.DTOs
+{
+    public sealed class ArtistGenreDto
+    {
+        public Guid GenreId { get; set; }
+        public bool IsPrimary { get; set; }
+        public ArtistGenreStatus Status { get; set; }
+    }
+}
