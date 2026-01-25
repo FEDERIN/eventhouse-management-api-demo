e1bfa3ee4fbf6c8236e89adf149d841f28733439
fpgutierrez <federingutierrez@gmail.com>
2026-01-25 15:33:24 +0100

    feat: add application command to set artist primary genre

---
+++ EventHouse.Management.Application/Commands/Artists/SetPrimaryGenre/SetPrimaryArtistGenreCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EventHouse.Management.Application.Commands.Artists.SetPrimaryGenre;
+
+internal sealed class SetPrimaryArtistGenreCommandValidator
+    : ArtistGenreCommandValidatorBase<SetPrimaryArtistGenreCommand>
+{
+    public SetPrimaryArtistGenreCommandValidator()
+    {
+        ApplyArtistGenreRules(
+            cmd => cmd.ArtistId,
+            cmd => cmd.GenreId);
+    }
+}
