44d8ad02ae2e2495e061043c92807566d22e0252
fpgutierrez <federingutierrez@gmail.com>
2026-02-21 19:22:16 +0100

    docs(swagger): enrich OpenAPI documentation with venue examples

---
+++ EventHouse.Management.Api/Swagger/Examples/Contracts/Venues/VenuePagedResultExample.cs
@@ -0,0 +1,24 @@
+﻿using EventHouse.Management.Api.Contracts.Common;
+using EventHouse.Management.Api.Contracts.Venues;
+using Swashbuckle.AspNetCore.Filters;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EventHouse.Management.Api.Swagger.Examples.Contracts.Venues;
+
+[ExcludeFromCodeCoverage]
+internal class VenuePagedResultExample : IExamplesProvider<PagedResult<Venue>>
+{
+    public PagedResult<Venue> GetExamples() => new()
+    {
+        Items = [VenueExampleData.MadisonSquareGarden()],
+        TotalCount = 1,
+        Page = 1,
+        PageSize = 20,
+        Links = new PaginationLinks
+        {
+            Self = "/api/v1/venues?page=1&pageSize=20",
+            First = "/api/v1/venues?page=1&pageSize=20",
+            Last = "/api/v1/venues?page=1&pageSize=20"
+        }
+    };
+}
