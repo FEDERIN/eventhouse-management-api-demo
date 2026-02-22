7f4caed2ae4cf26668773e1f62e21edd34821c0d
fpgutierrez <federingutierrez@gmail.com>
2026-02-22 11:53:11 +0100

    refactor/implement VenuesMapper Tdo(List<>)

--- EventHouse.Management.Application/Queries/Venues/GetById/GetVenueByIdQueryHandler.cs
+++ EventHouse.Management.Application/Queries/Venues/GetById/GetVenueByIdQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using EventHouse.Management.Application.Common.Interfaces;
 using EventHouse.Management.Application.DTOs;
 using EventHouse.Management.Application.Exceptions;
+using EventHouse.Management.Application.Mappers.Venues;
 using MediatR;
 
 namespace EventHouse.Management.Application.Queries.Venues.GetById;
@@ -15,18 +16,6 @@ internal sealed class GetVenueByIdQueryHandler(IVenueRepository repository)
         var entity = await _repository.GetByIdAsync(request.Id, cancellationToken)
             ?? throw new NotFoundException("Venue", request.Id);
 
-        return new VenueDto() {
-            Id = entity.Id,
-            Name = entity.Name,
-            Address = entity.Address,
-            City = entity.City,
-            Region = entity.Region,
-            CountryCode = entity.CountryCode,
-            Latitude = entity.Latitude,
-            Longitude = entity.Longitude,
-            Capacity = entity.Capacity,
-            TimeZoneId = entity.TimeZoneId,
-            IsActive = entity.IsActive
-        };
+        return VenuesMapper.ToDto(entity);
     }
 }
