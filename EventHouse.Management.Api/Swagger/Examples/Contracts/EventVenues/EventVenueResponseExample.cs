13538d4ec06a709ec08c3780b6b40c5008f3eb78
fpgutierrez <federingutierrez@gmail.com>
2026-03-08 09:08:13 +0100

    feact: add controller and documentation

---
+++ EventHouse.Management.Api/Swagger/Examples/Contracts/EventVenues/EventVenueResponseExample.cs
@@ -0,0 +1,11 @@
+﻿using EventHouse.Management.Api.Contracts.EventVenues;
+using Swashbuckle.AspNetCore.Filters;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EventHouse.Management.Api.Swagger.Examples.Contracts.EventVenues;
+
+[ExcludeFromCodeCoverage]
+internal sealed class EventVenueResponseExample : IExamplesProvider<EventVenueResponse>
+{ 
+    public EventVenueResponse GetExamples() => EventVenueExampleData.Result();
+}
