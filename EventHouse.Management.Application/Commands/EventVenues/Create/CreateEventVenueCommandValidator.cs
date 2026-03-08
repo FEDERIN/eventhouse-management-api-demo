0a4c5ffa19f9c575d086a9dc148ff44496455faa
fpgutierrez <federingutierrez@gmail.com>
2026-03-08 09:07:15 +0100

    feat: add application( command and handller)

---
+++ EventHouse.Management.Application/Commands/EventVenues/Create/CreateEventVenueCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace EventHouse.Management.Application.Commands.EventVenues.Create;
+
+internal sealed class CreateEventVenueCommandValidator
+    : EventVenueCommandValidatorBase<CreateEventVenueCommand>
+{
+    public CreateEventVenueCommandValidator()
+    {
+        RuleFor(x => x.EventId)
+            .NotEmpty().WithMessage("EventId no puede ser Guid.Empty.");
+
+        RuleFor(x => x.VenueId)
+            .NotEmpty().WithMessage("VenueId no puede ser Guid.Empty.");
+    }
+}
+
---
+++ EventHouse.Management.Application/Queries/EventVenues/GetById/GetEventVenueByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using EventHouse.Management.Application.Common.Interfaces;
+using EventHouse.Management.Application.DTOs;
+using EventHouse.Management.Application.Exceptions;
+using EventHouse.Management.Application.Mappers.EventVenues;
+using MediatR;
+
+namespace EventHouse.Management.Application.Queries.EventVenues.GetById;
+
+internal sealed class GetEventVenueByIdQueryHandler(IEventVenueRepository repository)
+            : IRequestHandler<GetEventVenueByIdQuery, EventVenueDto>
+{
+    public async Task<EventVenueDto> Handle(GetEventVenueByIdQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await repository.GetByIdAsync(request.Id, cancellationToken)
+                ?? throw new NotFoundException("EventVenue", request.Id);
+
+        return EventVenueMapper.ToDto(entity);
+    }
+}
