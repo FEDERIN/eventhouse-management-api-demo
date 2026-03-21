802b82ccf834b9afe42a6d1c3562d24f7402542f
fpgutierrez <federingutierrez@gmail.com>
2026-03-21 23:40:25 +0100

    test(api): introduce ApiEnumMapperUnidirectionalTestBase and refactor sort tests

---
+++ EventHouse.Management.Api.Tests/Mappers/ApiEnumMapperUnidirectionalTestBase.cs
@@ -0,0 +1,32 @@
+﻿namespace EventHouse.Management.Api.Tests.Mappers;
+
+public abstract class ApiEnumMapperUnidirectionalTestBase<TContract, TDto>
+    where TContract : struct, Enum
+    where TDto : struct, Enum
+{
+    protected abstract TDto ToApplicationRequired(TContract contract);
+    protected abstract TDto? ToApplicationOptional(TContract? contract);
+
+    [Fact]
+    public void ToApplicationRequired_WhenInvalidContract_ThrowsArgumentOutOfRangeException()
+    {
+        var invalid = (TContract)Enum.ToObject(typeof(TContract), 999);
+        Assert.Throws<ArgumentOutOfRangeException>(() => ToApplicationRequired(invalid));
+    }
+
+    [Fact]
+    public void ToApplicationOptional_WhenNull_ReturnsNull()
+    {
+        Assert.Null(ToApplicationOptional(null));
+    }
+
+    [Fact]
+    public void ToApplicationRequired_WhenValidContract_ReturnsMappedDto()
+    {
+        foreach (var contractValue in Enum.GetValues<TContract>())
+        {
+            var result = ToApplicationRequired(contractValue);
+            Assert.Equal(contractValue.ToString(), result.ToString());
+        }
+    }
+}
--- EventHouse.Management.Api.Tests/Mappers/VenueSortMapperTests.cs
+++ EventHouse.Management.Api.Tests/Mappers/VenueSortMapperTests.cs
@@ -5,7 +5,7 @@ using EventHouse.Management.Application.Queries.Venues.GetAll;
 namespace EventHouse.Management.Api.Tests.Mappers;
 
 public sealed class VenueSortMapperTests
-    : ApiEnumMapperTestBase<VenueSortBy, VenueSortField>
+    : ApiEnumMapperUnidirectionalTestBase<VenueSortBy, VenueSortField>
 {
     protected override VenueSortField ToApplicationRequired(VenueSortBy contract) =>
         VenueSortMapper.ToApplication(contract)
@@ -13,9 +13,4 @@ public sealed class VenueSortMapperTests
 
     protected override VenueSortField? ToApplicationOptional(VenueSortBy? contract) =>
         VenueSortMapper.ToApplication(contract);
-
-    protected override VenueSortBy ToContractRequired(VenueSortField dto)
-    {
-        throw new NotImplementedException("Sort mapping is unidirectional (Contract to App).");
-    }
 }
