6c05eb7c2196264a6e0a20ef382242e50c4ae11e
fpgutierrez <federingutierrez@gmail.com>
2026-03-22 17:59:42 +0100

    refactor: Mapper of EventVenue

--- EventHouse.Management.Application/Mappers/EventVenues/EventVenueStatusMapper.cs
+++ EventHouse.Management.Application/Mappers/EventVenues/EventVenueStatusMapper.cs
@@ -1,42 +1,16 @@
 ﻿
 using EventHouse.Management.Application.Common.Enums;
-using DomainStatus = EventHouse.Management.Domain.Enums.EventVenueStatus;
-
+using EventHouse.Management.Domain.Enums;
 namespace EventHouse.Management.Application.Mappers.EventVenues;
 
 public static class EventVenueStatusMapper
 {
-    public static DomainStatus ToDomainRequired(EventVenueStatusDto status) =>
-        status switch
-        {
-            EventVenueStatusDto.Active => DomainStatus.Active,
-            EventVenueStatusDto.Inactive => DomainStatus.Inactive,
-            _ => throw new ArgumentOutOfRangeException(
-                nameof(status),
-                status,
-                "Invalid EventVenueStatus value."
-            )
-        };
+    public static EventVenueStatus ToDomainRequired(EventVenueStatusDto dto) =>
+    EnumMapper<EventVenueStatus, EventVenueStatusDto>.ToDomainRequired(dto);
 
-    public static DomainStatus? ToDomainOptional(EventVenueStatusDto? status) =>
-        status switch
-        {
-            null => null,
-            EventVenueStatusDto.Active => DomainStatus.Active,
-            EventVenueStatusDto.Inactive => DomainStatus.Inactive,
-            _ => throw new ArgumentOutOfRangeException(
-                nameof(status),
-                status,
-                "Invalid EventVenueStatus value."
-            )
-        };
+    public static EventVenueStatus? ToDomainOptional(EventVenueStatusDto? dto) =>
+        EnumMapper<EventVenueStatus, EventVenueStatusDto>.ToDomainOptional(dto);
 
-    // Domain -> App
-    public static EventVenueStatusDto ToApplicationRequired(this DomainStatus status) =>
-        status switch
-        {
-            DomainStatus.Active => EventVenueStatusDto.Active,
-            DomainStatus.Inactive => EventVenueStatusDto.Inactive,
-            _ => throw new ArgumentOutOfRangeException(nameof(status), status, "Invalid DomainStatus value.")
-        };
+    public static EventVenueStatusDto ToApplicationRequired(EventVenueStatus domain) =>
+        EnumMapper<EventVenueStatus, EventVenueStatusDto>.ToApplicationRequired(domain);
 }
